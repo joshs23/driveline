282bce6565967c1b20ed6eb80b10b1ff88c51c36
Jellyton <julian@getfreemo.com>
2024-11-30 03:44:27 -0800

    convert root page at it's highest level to server component

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,19 +1,9 @@
-'use client';
-
-import { useAuth } from "../hooks/useAuth";
 import Feed from "./feed";
-import Login from "../login/login";
 
 export default function Home() {
-  const { isLoggedIn, loading } = useAuth();
-    
-  if (loading) {
-      return <div className="flex items-center justify-center min-h-screen">Loading...</div>;
-  }
-
   return (
-    <main className="flex items-center justify-center bg-purple-200 w-full min-h-screen">
-      {isLoggedIn ? <Feed /> : <Login onLoginSuccess={() => {}} />}
+    <main className="flex min-h-screen w-full items-center justify-center">
+      <Feed />
     </main>
   );
 }
