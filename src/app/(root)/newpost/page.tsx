738d929c01cbbd7a6987a8e09dfc7c065c7effaf
JonahB52 <138079618+JonahB52@users.noreply.github.com>
2024-11-30 06:40:57 -0800

    Added new post page & create post

---
+++ src/app/(root)/newpost/page.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import CreatePost from "./createpost";
+
+export default function NewPostPage() {
+    return (
+        <div className="w-full flex justify-center items-center min-h-screen bg-gray-100">
+            <CreatePost />
+        </div>
+    );
+}
