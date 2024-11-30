ef3a29cea4ec599cdc4b684ce517cc7bafd4d3d8
joshs23 <josh.sommer23@gmail.com>
2024-11-29 23:29:23 -0800

    returned supabase client instead of function

--- src/utils/supabase/client.ts
+++ src/utils/supabase/client.ts
@@ -1,6 +1,8 @@
 import { createBrowserClient } from "@supabase/ssr";
 import { Database } from "@/database.types";
 
-export function createClient() {
+function createClient() {
   return createBrowserClient<Database>(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!);
 }
+
+export const supabase = createClient();
