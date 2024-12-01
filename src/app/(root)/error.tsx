90ae7f8f5a58620ea0db8446b1e12eb24659d720
Jellyton <julian@getfreemo.com>
2024-11-30 21:22:48 -0800

    implement proper styled error route + global error route

--- src/app/(root)/error.tsx
+++ src/app/(root)/error.tsx
@@ -1,6 +1,8 @@
-"use client"; // Error boundaries must be Client Components
+"use client";
+import { Button } from "@/components/ui/button";
+import { CircleAlert } from "lucide-react";
 
-export default function GlobalError({
+export default function Error({
   error,
   reset,
 }: {
@@ -8,12 +10,15 @@ export default function GlobalError({
   reset: () => void;
 }) {
   return (
-    // global-error must include html and body tags
-    <html>
-      <body>
-        <h2>Something went wrong!</h2>
-        <button onClick={() => reset()}>Try again</button>
-      </body>
-    </html>
+    <div className="flex size-full min-h-screen grow flex-col items-center justify-center gap-2">
+      <div className="flex flex-col items-center justify-center gap-2 rounded-md border p-8 shadow-lg">
+        <CircleAlert size={64} />
+        <h1 className="scroll-m-20 text-4xl font-bold tracking-tight">
+          An Error Occured
+        </h1>
+
+        <Button onClick={() => reset()}>Reload</Button>
+      </div>
+    </div>
   );
 }
--- src/app/global-error.tsx
+++ src/app/global-error.tsx
@@ -1,4 +1,6 @@
-"use client"; // Error boundaries must be Client Components
+"use client";
+import { Button } from "@/components/ui/button";
+import { CircleAlert } from "lucide-react";
 
 export default function GlobalError({
   error,
@@ -8,11 +10,16 @@ export default function GlobalError({
   reset: () => void;
 }) {
   return (
-    // global-error must include html and body tags
     <html>
-      <body>
-        <h2>Something went wrong!</h2>
-        <button onClick={() => reset()}>Try again</button>
+      <body className="flex size-full min-h-screen grow flex-col items-center justify-center gap-2">
+        <div className="flex flex-col items-center justify-center gap-2 rounded-md border p-8 shadow-lg">
+          <CircleAlert size={64} />
+          <h1 className="scroll-m-20 text-4xl font-bold tracking-tight">
+            An Error Occured
+          </h1>
+
+          <Button onClick={() => reset()}>Reload</Button>
+        </div>
       </body>
     </html>
   );
