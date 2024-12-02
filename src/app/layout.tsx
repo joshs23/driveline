fcc51f124dd25dbefa250edf0777a71baf7269bd
Jellyton <julian@getfreemo.com>
2024-12-02 00:18:33 -0800

    set dark mode

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,7 @@ export default async function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body
-        className={`${GeistSans.className} antialiased`}
+        className={`${GeistSans.className} dark antialiased`}
         suppressHydrationWarning
       >
         <Providers>
