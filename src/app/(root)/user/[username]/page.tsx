f30933c440b647de16ecfa75ebd2cdc41120f5eb
Jellyton <julian@getfreemo.com>
2024-12-03 04:23:51 -0800

    add post feed to user profiles

--- src/app/(root)/user/[username]/page.tsx
+++ src/app/(root)/user/[username]/page.tsx
@@ -1,5 +1,8 @@
+import { createClient } from "@/utils/supabase/server";
 import UserPage from "./user-page";
 import Vehicles from "./vehicles";
+import Feed from "../../feed";
+import { notFound } from "next/navigation";
 
 export default async function Page({
   params,
@@ -15,10 +18,40 @@ export default async function Page({
       </div>
     );
 
+  const supabase = await createClient();
+
+  const { data: user, error: userError } = await supabase
+    .from("UserProfile")
+    .select("*")
+    .eq("username", username)
+    .single();
+
+  if (userError) {
+    console.error("Error fetching profile:", userError);
+    return notFound();
+  }
+
+  const { data, error } = await supabase
+    .from("Post")
+    .select("*, PostImage(*)")
+    .eq("creator", user.user_id)
+    .order("id", { ascending: false });
+  if (error) {
+    console.error(error);
+  }
+
   return (
-    <main className="min-h-screen w-full flex-auto items-center justify-center bg-secondary">
+    <main className="flex h-screen w-full flex-col bg-secondary">
       <UserPage username={username} />
-      <Vehicles username={username} />
+
+      <div className="grid w-full grow grid-cols-2 py-4">
+        <div className="flex w-full flex-col gap-4 border-r">
+          <h1 className="px-6 pt-4 text-3xl font-bold">Feed</h1>
+          <Feed initalPosts={data} disableCreatePost inline />
+        </div>
+
+        <Vehicles username={username} />
+      </div>
     </main>
   );
 }
