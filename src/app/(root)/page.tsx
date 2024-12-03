32f7ff5ed90e6d37c01b47b78be974cc022564e4
joshs23 <josh.sommer23@gmail.com>
2024-12-03 12:26:04 -0800

    Added Comments to data

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -5,7 +5,7 @@ export default async function Home() {
   const supabase = await createClient();
   const { data, error } = await supabase
     .from("Post")
-    .select("*, PostImage(*)")
+    .select("*, PostImage(*), Comment(*)")
     .order("id", { ascending: false });
   if (error) {
     console.error(error);
