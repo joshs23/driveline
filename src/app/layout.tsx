6a840a4148fc8863b05222b48d57e298f84b1d68
Jellyton <julian@getfreemo.com>
2024-12-03 19:48:45 -0800

    rich colors for toasts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,7 +30,7 @@ export default async function RootLayout({
             {children}
           </div>
         </Providers>
-        <Toaster />
+        <Toaster richColors />
       </body>
     </html>
   );
