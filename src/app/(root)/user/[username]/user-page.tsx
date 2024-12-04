26d98393242c0fe30f4aed8be6eb9ea98da15de8
Jellyton <julian@getfreemo.com>
2024-12-03 19:46:31 -0800

    add uploading profile pictures system

--- src/app/(root)/user/[username]/user-page.tsx
+++ src/app/(root)/user/[username]/user-page.tsx
@@ -6,6 +6,7 @@ import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { LoaderCircle } from "lucide-react";
 import { notFound, redirect, usePathname } from "next/navigation";
 import { useEffect, useRef, useState } from "react";
+import { toast } from "sonner";
 
 const projectId =
   process.env.NEXT_PUBLIC_SUPABASE_URL?.split("//")[1].split(".")[0];
@@ -101,7 +102,106 @@ function Banner({ userDetails }: { userDetails: Tables<"UserProfile"> }) {
   );
 }
 
-//somizlnspycsfofvmxjs.supabase.co/storage/v1/object/public/avatars/6/Tuning-Car-PNG-Image-File.png
+function ProfilePicture({
+  userDetails,
+}: {
+  userDetails: Tables<"UserProfile">;
+}) {
+  const [currentUserId, setCurrentUserId] = useState<string>();
+  const fileInputRef = useRef<HTMLInputElement>(null);
+  const pathname = usePathname();
+  const queryClient = useQueryClient();
+
+  useEffect(() => {
+    getCurrentUser().then((data) => setCurrentUserId(data?.user?.id));
+  }, []);
+
+  const handleImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (!event.target.files) return;
+    uploadAvatar(event.target.files[0]);
+  };
+
+  async function uploadAvatar(avatarImage: File) {
+    if (!avatarImage) return;
+
+    const toastId = toast.loading("Uploading new avatar...", {
+      position: "top-right",
+    });
+
+    const supabase = createClient();
+    const { data: imageData, error } = await supabase.storage
+      .from("avatars")
+      .upload(
+        `${userDetails.id}/avatar/${sanitizeFileName(avatarImage.name)}`,
+        avatarImage,
+        {
+          upsert: true,
+        },
+      );
+
+    if (error) {
+      toast.error("Error uploading image! - " + error.message, {
+        id: toastId,
+      });
+      console.error("Error uploading image:", error);
+      return;
+    }
+
+    if (imageData) {
+      await supabase
+        .from("UserProfile")
+        .update({
+          profile_picture_url: imageData.path,
+        })
+        .eq("user_id", userDetails.user_id);
+    }
+
+    toast.success("Avatar has been updated!", {
+      id: toastId,
+      className: "bg-green-600",
+    });
+
+    queryClient.invalidateQueries({
+      queryKey: ["username", userDetails.username],
+    });
+
+    redirect(pathname);
+  }
+
+  return (
+    <div
+      className={cn(
+        currentUserId &&
+          currentUserId == userDetails.user_id &&
+          "cursor-pointer",
+        "absolute -bottom-16 left-1/2 h-32 w-32 -translate-x-1/2 transform sm:h-40 sm:w-40",
+      )}
+      onClick={() => {
+        if (!currentUserId || currentUserId !== userDetails.user_id) return;
+        fileInputRef.current?.click();
+      }}
+    >
+      <input
+        type="file"
+        ref={fileInputRef}
+        onChange={handleImageChange}
+        className="hidden"
+      />
+
+      {userDetails.profile_picture_url ? (
+        <img
+          src={`https://${projectId}.supabase.co/storage/v1/object/public/avatars/${userDetails.profile_picture_url}`}
+          alt="Profile Picture"
+          className="h-full w-full rounded-full border-4 border-border bg-secondary object-cover shadow-md"
+        />
+      ) : (
+        <div className="flex h-full w-full items-center justify-center rounded-full border-4 border-white bg-gray-300 shadow-md">
+          <p className="text-gray-500">No Profile Pic</p>
+        </div>
+      )}
+    </div>
+  );
+}
 
 export default function UserPage({ username }: { username: string }) {
   const { isPending, data: userDetails } = useQuery({
@@ -129,30 +229,16 @@ export default function UserPage({ username }: { username: string }) {
 
   if (!userDetails) notFound();
 
-  const { display_name, profile_picture_url } = userDetails;
-
   return (
     <div className="w-full">
       {/* Header Section */}
       <div className="relative h-[25rem] w-full border-b border-zinc-600">
         <Banner userDetails={userDetails} />
-        <div className="absolute -bottom-16 left-1/2 h-32 w-32 -translate-x-1/2 transform sm:h-40 sm:w-40">
-          {profile_picture_url ? (
-            <img
-              src={profile_picture_url}
-              alt="Profile"
-              className="h-full w-full rounded-full border-4 border-white object-cover shadow-md"
-            />
-          ) : (
-            <div className="flex h-full w-full items-center justify-center rounded-full border-4 border-white bg-gray-300 shadow-md">
-              <p className="text-gray-500">No Profile Pic</p>
-            </div>
-          )}
-        </div>
+        <ProfilePicture userDetails={userDetails} />
       </div>
       {/* User Info Section */}
       <div className="mt-20 text-center">
-        <h1 className="text-2xl font-semibold">{display_name}</h1>
+        <h1 className="text-2xl font-semibold">{userDetails.display_name}</h1>
         <p className="text-lg">@{username}</p>
       </div>
     </div>
