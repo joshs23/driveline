24e058562802a07e3566b6f67133a23b8c2a2b16
Jellyton <julian@getfreemo.com>
2024-12-10 19:31:48 -0800

    update styling of communities page to flow better, and properly change grid cols at smaller window sizes

--- src/app/(root)/communities/communities.tsx
+++ src/app/(root)/communities/communities.tsx
@@ -1,5 +1,6 @@
 import { createClient } from "@/utils/supabase/server";
 import Link from "next/link";
+import CreateCommunity from "./create-community";
 import { Separator } from "@/components/ui/separator";
 
 export default async function Communities() {
@@ -12,21 +13,27 @@ export default async function Communities() {
     return <div>There are no communities. Create one!</div>;
 
   return (
-    <div className="container w-fit py-8">
+    <div className="container mx-auto w-fit px-4 py-2">
       <div className="flex gap-4">
         <h1 className="mb-2 text-3xl font-semibold">Communities</h1>
         <CreateCommunity />
       </div>
       <p className="mb-4 opacity-70">Find your people!</p>
-      <div className="gap-4 grid grid-cols-4">
-        {listings.map((listing) => (
-          <Link href={`/communities/${listing.id}`} passHref key={listing.name}>
-            <div className="min-h-48 min-w-48 max-w-96 rounded-lg border-2 p-4 shadow-md transition-shadow duration-300 hover:bg-secondary">
-              <h1 className="mb-2 text-xl font-semibold">{listing.name}</h1>
-              <h2 className="text-md mb-1 font-medium">
-                Members: {listing.count}
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-2 xl:grid-cols-4">
+        {communities.map((listing) => (
+          <Link
+            key={listing.name}
+            href={`/communities/${listing.id}`}
+            passHref
+            legacyBehavior
+          >
+            <div className="flex min-w-48 max-w-96 cursor-pointer flex-col gap-1 rounded-lg border-2 p-4 shadow-md transition-shadow duration-300 hover:bg-secondary">
+              <h1 className="text-2xl font-bold">{listing.name}</h1>
+              <h2 className="text-md font-semibold">
+                Members: {listing.CommunityMember[0]?.count || "None"}
               </h2>
-              <p className="mb-4">{listing.description}</p>
+              <Separator className="my-1 bg-neutral-600" />
+              <p className="">{listing.description}</p>
             </div>
           </Link>
         ))}
