10b5959d951f40abef58f707ec7a42218576ab4b
Jellyton <julian@getfreemo.com>
2024-12-10 19:37:27 -0800

    Major Changes to Community/[id] route - Convert entire page to a server component - Removed all usage of hooks - Major downsizing of sequential queries, including the implementation of a "view" in the database, to simplify several queries into one, by using advanced joins - converted onClick event handlers to inline server actions - major improvements to general styling and layout, fix height overflow - simplified several render conditional checks

--- src/app/(root)/communities/[id]/page.tsx
+++ src/app/(root)/communities/[id]/page.tsx
@@ -1,204 +1,153 @@
-"use client";
-
-import { useEffect, useState } from "react";
-import { createClient } from "@/utils/supabase/client";
-import { notFound } from "next/navigation";
+import { createClient } from "@/utils/supabase/server";
+import { notFound, redirect } from "next/navigation";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import Feed from "@/app/(root)/feed";
+import { Separator } from "@/components/ui/separator";
 
-interface Community {
-  id: number;
-  name: string;
-  description: string | null;
-}
+const projectId =
+  process.env.NEXT_PUBLIC_SUPABASE_URL?.split("//")[1].split(".")[0];
 
-interface Member {
-  username: string;
-  display_name: string;
-  profile_picture_url: string | null;
-}
-
-interface Post {
-  id: number;
-  body: string;
-  community: number | null;
-  created_at: string;
-  creator: string;
-  content: string;
-  author: string;
-  createdAt: string;
-}
-
-export default function Page({
+export default async function Page({
   params,
 }: {
-  params: Promise<{ id: string }>;
+  params: Promise<{ id: number }>;
 }) {
-  const [amMember, setAmMember] = useState(false);
-  const [community, setCommunity] = useState<Community | null>(null);
-  const [members, setMembers] = useState<Member[]>([]);
-  const [memberProfiles, setMemberProfiles] = useState<Member[]>([]);
-  const [posts, setPosts] = useState<Post[]>([]);
-  const [memberIds, setMemberIds] = useState<string[]>([]);
+  const { id } = await params;
+  const supabase = await createClient();
 
+  const { data: community } = await supabase
+    .from("Community")
+    .select("*")
+    .eq("id", id)
+    .single();
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const { id } = await params;
-      const supabase = await createClient();
+  if (!community) {
+    return notFound();
+  }
 
-      const { data: communityData } = await supabase
-        .from("Community")
-        .select("*")
-        .eq("id", id)
-        .single();
+  const { data: members } = await supabase
+    .from("CommunityMemberWithProfile")
+    .select("*")
+    .eq("community_id", id);
 
-      if (!communityData) {
-        return notFound();
-      }
+  const user = await supabase.auth.getUser();
+  const userId = user.data.user?.id;
 
-      const { data: memberIds } = await supabase
-        .from("CommunityMember")
-        .select("user_id")
-        .eq("community_id", id);
-
-      const { data: membersData } = await supabase
-        .from("UserProfile")
-        .select("*")
-        .in("user_id", memberIds?.map((member) => member.user_id) || []);
-
-      const user = await supabase.auth.getUser();
-      const userId = user.data.user?.id;
+  const isMember = !!members?.find((member) => member.user_id === userId);
 
   const { data: rawPosts } = await supabase
-        .from("Post")
-        .select("*, PostImage(*), Comment(*)")
+    .from("Post")
+    .select("*, PostImage(*), Comment(*)")
     .eq("community", id)
-        .order("id", { ascending: false });
+    .order("id", { ascending: false });
 
   const posts =
     rawPosts?.map((post) => ({
-          ...post,
-          Comment: Array.isArray(post.Comment) ? post.Comment : [],
-          content: post.body,
-          author: post.creator,
-          createdAt: post.created_at,
-        })) || [];
+      ...post,
+      Comment: Array.isArray(post.Comment) ? post.Comment : [],
+      content: post.body,
+      author: post.creator,
+      createdAt: post.created_at,
+    })) || [];
 
   const handleJoin = async () => {
+    "use server";
     const supabase = await createClient();
     const user = await supabase.auth.getUser();
     const userId = user.data.user?.id;
 
     if (!userId || !community?.id) return;
 
-    const { data } = await supabase
+    await supabase
       .from("CommunityMember")
       .insert([{ community_id: Number(community.id), user_id: userId }]);
 
-    window.location.reload();
+    redirect(`/communities/${community.id}`);
   };
 
   const handleLeave = async () => {
+    "use server";
     const supabase = await createClient();
     const user = await supabase.auth.getUser();
     const userId = user.data.user?.id;
 
     if (!userId || !community?.id) return;
 
-    const { data } = await supabase
+    await supabase
       .from("CommunityMember")
       .delete()
       .eq("community_id", Number(community.id))
       .eq("user_id", userId);
 
-    window.location.reload();
+    redirect(`/communities/${community.id}`);
   };
 
   return (
-    <div className="container mx-auto px-4 ">
-      <div className="rounded-lg p-6">
-        <div className="flex items-center justify-between border-4 p-4">
-          {/* Community Header */}
-          <div>
-            <h1 className="mb-4 text-3xl font-bold text-primary">
-              {community?.name}
-            </h1>
-            <p className="mb-6">{community?.description}</p>
-          </div>
-          {/* Actions Section */}
-          <div className="p-6">
-            {/* Join/Leave buttons */}
-            {!amMember ? (
-              <Button
-                onClick={handleJoin}
-                className="rounded-md bg-primary px-4 text-white"
-              >
-                Join Community
-              </Button>
-            ) : (
-              <Button
-                onClick={handleLeave}
-                className="rounded-md bg-primary px-4 text-white"
-              >
-                Leave Community
-              </Button>
-            )}
-          </div>
+    <div className="container flex max-h-screen w-full flex-col gap-4 rounded-lg px-4 py-6">
+      <div className="flex flex-col justify-between gap-4 p-4">
+        {/* Community Header */}
+        <div className="flex flex-col gap-2">
+          <h1 className="text-4xl font-bold">{community?.name}</h1>
+          <Separator className="my-2 bg-neutral-600" />
+          <p>{community?.description}</p>
         </div>
-        <div className="flex gap-8">
-          {/* Feed */}
+
+        {/* Join/Leave buttons */}
+        <Button
+          onClick={!isMember ? handleJoin : handleLeave}
+          className="rounded-md bg-primary/20 px-4 text-red-400 hover:bg-primary/10"
+        >
+          {isMember ? "Leave" : "Join"} Community
+        </Button>
+      </div>
+      <div className="flex h-full gap-8">
+        {/* Feed */}
+        <div className="flex grow flex-col gap-2 rounded-md border p-4 shadow-lg">
+          <h1 className="text-3xl font-bold">Feed</h1>
           <Feed initalPosts={posts} communityId={id} inline />
-          </div>
-          {/* Members Section */}
-          <div className="mt-4 items-center justify-between border-4 p-4 w-1/4">
-            <h2 className="mb-3 p-4 text-2xl font-semibold">Members</h2>
-            {(members.length ?? 0) <= 0 ? (
-              <p className="italic">There are no members in this community.</p>
-            ) : (
-              <ul className="space-y-2">
-                {memberProfiles?.map(
-                  (memberProfile: {
-                    username: string;
-                    display_name: string;
-                    profile_picture_url: string | null;
-                  }) => (
-                    <li
-                      key={memberProfile.username}
-                      className="items-center rounded-md bg-card p-2 shadow-lg transition-shadow duration-300 hover:bg-primary"
-                    >
-                      <Link href={`/user/${memberProfile.username}`}>
-                        <div className="flex">
-                          <Avatar className="h-12 w-12 flex-shrink-0">
-                            <AvatarImage
-                              src={
-                                (memberProfile?.profile_picture_url &&
-                                  `https://${projectId}.supabase.co/storage/v1/object/public/avatars/${memberProfile.profile_picture_url}`) ||
-                                undefined
-                              }
-                              alt={`${memberProfile.display_name}'s avatar`}
-                              className="rounded-full"
-                            />
-                            <AvatarFallback className="flex h-full w-full items-center justify-center rounded-full font-bold">
-                              {memberProfile.display_name.charAt(0)}
-                            </AvatarFallback>
-                          </Avatar>
-                          <div className="px-4">
-                            <p className="text-lg font-medium">
-                              {memberProfile.display_name}
-                            </p>
-                            <p className="text-sm">@{memberProfile.username}</p>
-                          </div>
-                        </div>
-                      </Link>
-                    </li>
-                  ),
-                )}
-              </ul>
-            )}
-          </div>
+        </div>
+        {/* Members Section */}
+        <div className="flex w-1/4 flex-col gap-2 rounded-md border p-4 shadow-lg">
+          <h2 className="text-3xl font-bold">Members</h2>
+          {(members?.length ?? 0) <= 0 ? (
+            <p className="italic">There are no members in this community.</p>
+          ) : (
+            <div className="flex flex-col gap-2">
+              {members?.map((member) => (
+                <Link
+                  key={member.username}
+                  href={`/user/${member.username}`}
+                  passHref
+                  legacyBehavior
+                >
+                  <div className="hover flex cursor-pointer items-center rounded-md border p-2 shadow-lg transition-colors duration-200 hover:bg-neutral-900">
+                    <Avatar className="h-12 w-12 flex-shrink-0">
+                      <AvatarImage
+                        src={
+                          (member?.profile_picture_url &&
+                            `https://${projectId}.supabase.co/storage/v1/object/public/avatars/${member.profile_picture_url}`) ||
+                          undefined
+                        }
+                        alt={`${member.display_name}'s avatar`}
+                        className="rounded-full"
+                      />
+                      <AvatarFallback className="flex h-full w-full items-center justify-center rounded-full font-bold">
+                        {member.display_name?.charAt(0)}
+                      </AvatarFallback>
+                    </Avatar>
+                    <div className="px-4">
+                      <p className="text-lg font-medium">
+                        {member.display_name}
+                      </p>
+                      <p className="text-sm">@{member.username}</p>
+                    </div>
+                  </div>
+                </Link>
+              ))}
+            </div>
+          )}
         </div>
       </div>
     </div>
