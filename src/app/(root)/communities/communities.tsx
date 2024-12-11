14099e114c590a2ae91b4375104e913208b385e6
Jellyton <julian@getfreemo.com>
2024-12-10 19:31:25 -0800

    update communities page to properly query member count using aggregation query

--- src/app/(root)/communities/communities.tsx
+++ src/app/(root)/communities/communities.tsx
@@ -1,32 +1,15 @@
 import { createClient } from "@/utils/supabase/server";
 import Link from "next/link";
-import CreateCommunity from "./createcommunity";
-
-async function countMembers(communityId: number) {
-  const supabase = await createClient();
-  const { data, error } = await supabase
-    .from("CommunityMember")
-    .select("user_id")
-    .eq("community_id", communityId);
-
-  if (error) throw error;
-
-  return data?.length || 0;
-}
+import { Separator } from "@/components/ui/separator";
 
 export default async function Communities() {
   const supabase = await createClient();
-  const { data: communities } = await supabase.from("Community").select("*");
+  const { data: communities } = await supabase
+    .from("Community")
+    .select("*, CommunityMember(count)");
 
   if (!communities || communities.length <= 0)
     return <div>There are no communities. Create one!</div>;
-
-  const listings = communities.map((community) => ({
-    id: community.id,
-    name: community.name,
-    description: community.description,
-    count: countMembers(community.id),
-  }));
 
   return (
     <div className="container w-fit py-8">
